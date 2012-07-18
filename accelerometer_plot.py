1b9c14b88455baea29ebcba3671c2025ae541765
Phil Oakley <phil.oakley@gmail.com>
2012-07-18 17:33:44 -0400

    Basic Functionality
    Gets accelerometer data and plots 1 axis. This doesn't yet work with
    multiple axes, and the data is uncalibrated.

---
+++ accelerometer_plot.py
@@ -0,0 +1,84 @@
+import serial as sr
+import numpy as np
+from matplotlib.lines import Line2D
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class Scope:
+    def __init__(self, ax, maxt=500, dt=1):
+        #Initialize the plot - run once
+        self.ax = ax
+        self.dt = dt
+        self.maxt = maxt
+        
+        #Zero the data
+        self.tdata = [0]
+        self.ydata = [0]
+        
+        #Create the line
+        self.line = Line2D(self.tdata, self.ydata)
+        self.ax.add_line(self.line)
+        
+        #Set the x and y graph limits
+        self.ax.set_ylim(-.1, 1000)
+        self.ax.set_xlim(0, self.maxt)
+
+    def update(self, y):
+    	#Update the plot
+    	
+    	#Get the last timestep
+        lastt = self.tdata[-1]
+        
+        #If the end of the graph is reached, reset the arrays
+        if lastt > self.tdata[0] + self.maxt: # reset the arrays
+            self.tdata = [self.tdata[-1]]
+            self.ydata = [self.ydata[-1]]
+            self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
+            self.ax.figure.canvas.draw()
+
+	#Set the new t value to the dt more than the largest existing t value
+        t = self.tdata[-1] + self.dt
+        
+        #Add to the existing time array
+        self.tdata.append(t)
+        
+        #Update the existing y array with the value passed in to the function
+        self.ydata.append(y)
+        
+        #Update the line
+        self.line.set_data(self.tdata, self.ydata)
+        return self.line,
+
+
+def emitter():
+
+	#Read all the data in the buffer (maxes out at 128 bytes)
+	a=ser.read(ser.inWaiting())
+	
+	#Split them based on new lines
+	lines = a.split('\n')
+	
+	#Select the second to last line (sometimes the very last line is empty)
+	#Beware - this throws away all the other data
+	buffer = lines[-2]
+	yield buffer
+	
+
+#Set up the serial feed
+ser = sr.Serial('/dev/tty.usbmodem621', 9600)
+
+#Create the figure
+fig = plt.figure()
+
+#Create the main subplot
+ax = fig.add_subplot(111)
+
+#Create the scope class
+scope = Scope(ax)
+
+# pass a generator in "emitter" to produce data for the update function every 2 milliseconds
+ani = animation.FuncAnimation(fig, scope.update, emitter, interval=2,
+    blit=True)
+
+#Create the plot window
+plt.show()
